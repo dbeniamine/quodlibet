f71099feeca63e991ad151f16c152bcad62cb915
David Beniamine <david@beniamine.net>
2021-04-06 23:23:07 +0200

    Install dependencies with poetry

--- .github/workflows/debian11/entrypoint.sh
+++ .github/workflows/debian11/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-dbus-daemon --session &
-PYTEST_ADDOPTS=-rxXs python3 setup.py test
+#dbus-daemon --session &
+# Fix for hanging flake8 test
+# https://github.com/quodlibet/quodlibet/issues/3539#issuecomment-767389459
+poetry config virtualenvs.in-project true
+
+poetry install -E plugins
+sudo -u user poetry run python3 setup.py test
